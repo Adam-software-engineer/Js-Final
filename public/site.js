ab405db20d030d9f9120bbf7f1d13556cd2975f5
james <300089902@fvtc.edu>
2024-05-01 15:46:52 -0500

    Added iframe and sticky navbar

--- public/site.js
+++ public/site.js
@@ -5,3 +5,18 @@ async () => {
   const input = document.querySelector("input");
   const ul = document.querySelector("ul");
 };
+
+
+// CSS JS for webpage nev
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("Nav");
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the nav when you scroll
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
